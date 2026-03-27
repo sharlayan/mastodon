9a0f60faf198c8e8d549e0f52b4fcd51913a89cb
Arcturus <github@drk.st>
2026-03-27 12:35:14 +0000

    [sharlayan] migrate error fix (on test env)

--- db/migrate/20260324210822_add_push_forward_to_account_switch_authorizations.rb
+++ db/migrate/20260324210822_add_push_forward_to_account_switch_authorizations.rb
@@ -2,6 +2,8 @@
 
 class AddPushForwardToAccountSwitchAuthorizations < ActiveRecord::Migration[7.2]
   def change
+    return if column_exists?(:account_switch_authorizations, :push_forward)
+
     add_column :account_switch_authorizations, :push_forward, :boolean, default: false, null: false
   end
 end
