a601b1804fdab0b8e81c7b811155dd419e149d75
Arcturus <github@drk.st>
2026-06-25 17:10:00 +0000

    [Sharlayan] fix missing schema migrate for running instances (old migrate) and fix ja locale

---
+++ db/migrate/20260626020400_fix_missing_share_key_on_generated_annual_reports.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class FixMissingShareKeyOnGeneratedAnnualReports < ActiveRecord::Migration[8.1]
+  def up
+    add_column :generated_annual_reports, :share_key, :string unless column_exists?(:generated_annual_reports, :share_key)
+
+    add_column :collections, :item_count, :integer, default: 0, null: false unless column_exists?(:collections, :item_count)
+  end
+
+  def down
+    # no-op: columns are part of the canonical schema
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_25_213900) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_26_020400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -524,6 +524,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_25_213900) do
     t.text "description"
     t.text "description_html"
     t.boolean "discoverable", null: false
+    t.integer "item_count", default: 0, null: false
     t.string "language"
     t.boolean "local", null: false
     t.string "name", null: false
@@ -818,6 +819,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_25_213900) do
     t.datetime "created_at", null: false
     t.jsonb "data", null: false
     t.integer "schema_version", null: false
+    t.string "share_key"
     t.datetime "updated_at", null: false
     t.datetime "viewed_at"
     t.integer "year", null: false
