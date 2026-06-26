63ee8c48e4ba5cb5097a9c6b693bdfc3442b3dc4
Arcturus <github@drk.st>
2026-06-26 06:43:22 +0000

    [Sharlayan] fix eslint and edit readme

--- app/javascript/flavours/glitch/features/clips/timeline.tsx
+++ app/javascript/flavours/glitch/features/clips/timeline.tsx
@@ -16,7 +16,7 @@ import { Column } from 'flavours/glitch/components/column';
 import { ColumnHeader } from 'flavours/glitch/components/column_header';
 import { Icon } from 'flavours/glitch/components/icon';
 import { LoadingIndicator } from 'flavours/glitch/components/loading_indicator';
-import BundleColumnError from 'flavours/glitch/features/ui/components/bundle_column_error';
+import { BundleColumnError } from 'flavours/glitch/features/ui/components/bundle_column_error';
 import StatusListContainer from 'flavours/glitch/features/ui/containers/status_list_container';
 import { useAppDispatch, useAppSelector } from 'flavours/glitch/store';
 
