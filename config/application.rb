39dd55dd124c26ab1db9c359e24b041dd15d4918
Arcturus <github@drk.st>
2026-07-01 10:51:52 +0000

    [Sharlayan] block all federation

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ require_relative '../lib/paperclip/response_with_limit_adapter'
 require_relative '../lib/terrapin/multi_pipe_extensions'
 require_relative '../lib/mastodon/middleware/public_file_server'
 require_relative '../lib/mastodon/middleware/socket_cleanup'
+require_relative '../lib/mastodon/middleware/roleplay_federation_block'
 require_relative '../lib/mastodon/email_configuration_helper'
 require_relative '../lib/mastodon/feature'
 require_relative '../lib/mastodon/snowflake'
@@ -88,6 +89,7 @@ module Mastodon
 
     config.middleware.use Mastodon::Middleware::PublicFileServer if Rails.env.local? || ENV['RAILS_SERVE_STATIC_FILES'] == 'true'
     config.middleware.use Rack::Attack
+    config.middleware.use Mastodon::Middleware::RoleplayFederationBlock
     config.middleware.use Mastodon::Middleware::SocketCleanup
 
     config.before_configuration do
