3758bb42f15d5064cc74dd910a57359bf34389b7
Arcturus <github@drk.st>
2026-07-06 15:07:53 +0000

    [Sharlayan] hostname remove from badge on community mode

--- app/javascript/flavours/glitch/components/account_header/badges.tsx
+++ app/javascript/flavours/glitch/components/account_header/badges.tsx
@@ -5,6 +5,7 @@ import { FormattedMessage } from 'react-intl';
 
 import { fetchRelationships } from '@/flavours/glitch/actions/accounts';
 import { useAccount } from '@/flavours/glitch/hooks/useAccount';
+import { roleplayMode } from '@/flavours/glitch/initial_state';
 import type { AccountRole } from '@/flavours/glitch/models/account';
 import { useAppDispatch, useAppSelector } from '@/flavours/glitch/store';
 
@@ -45,12 +46,13 @@ export const AccountBadges: FC<{ accountId: string }> = ({ accountId }) => {
     ? account.acct.split('@')[1]
     : localDomain;
   account.roles.forEach((role) => {
+    const roleDomain = roleplayMode ? undefined : `(${domain})`;
     if (isAdminBadge(role)) {
       badges.push(
         <AdminBadge
           key={role.id}
           label={role.name}
-          domain={`(${domain})`}
+          domain={roleDomain}
           roleId={role.id}
           color={role.color}
         />,
@@ -60,7 +62,7 @@ export const AccountBadges: FC<{ accountId: string }> = ({ accountId }) => {
         <Badge
           key={role.id}
           label={role.name}
-          domain={`(${domain})`}
+          domain={roleDomain}
           roleId={role.id}
           color={role.color}
         />,
