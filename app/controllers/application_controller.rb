9368b903d6a20d76a3268c8597d788c33731e6e2
Sharlayan <github@drk.st>
2026-07-09 22:33:58 +0000

    [Sharlayan] Fix for Pskey CSP error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
   include DatabaseHelper
   include AuthorizedFetchHelper
   include SelfDestructHelper
+  include MisskeyCompat::PskeyCspConcern
 
   helper_method :current_account
   helper_method :current_session
