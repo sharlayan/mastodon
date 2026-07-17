1c6822e25277441dc4aeddd2623d296c59afdc33
Sharlayan <github@drk.st>
2026-07-17 21:14:31 +0000

    [Sharlayan] Optimize reaction queries

--- app/models/concerns/sharlayan/status/reactions.rb
+++ app/models/concerns/sharlayan/status/reactions.rb
@@ -27,10 +27,32 @@ module Sharlayan::Status::Reactions
         ).to_a
 
       ActiveRecord::Associations::Preloader.new(records: records, associations: { custom_emoji: :local_counterpart }).call
+      preload_reaction_accounts(records, status_ids, excluded_account_ids)
       records.group_by(&:status_id)
     end
 
     private
+
+    def preload_reaction_accounts(records, status_ids, excluded_account_ids)
+      return if records.empty?
+
+      rows_scope = StatusReaction.where(status_id: status_ids)
+      rows_scope = rows_scope.where.not(account_id: excluded_account_ids) if excluded_account_ids.present?
+
+      grouped_account_ids = Hash.new { |hash, key| hash[key] = [] }
+      rows_scope.order(:id).pluck(:status_id, :name, :custom_emoji_id, :account_id).each do |status_id, name, custom_emoji_id, account_id|
+        grouped_account_ids[[status_id, name, custom_emoji_id]] << account_id
+      end
+
+      needed_ids = grouped_account_ids.values.flat_map { |ids| ids.first(StatusReaction::USERS_DISPLAY_LIMIT) }.uniq
+      accounts_by_id = Account.where(id: needed_ids).index_by(&:id)
+
+      records.each do |record|
+        ids = grouped_account_ids[[record.status_id, record.name, record.custom_emoji_id]]
+        record.preloaded_account_ids = ids
+        record.preloaded_users = ids.first(StatusReaction::USERS_DISPLAY_LIMIT).filter_map { |id| accounts_by_id[id] }
+      end
+    end
 
     def value_for_reaction_me_column(account_id)
       return 'FALSE AS me' if account_id.nil?
