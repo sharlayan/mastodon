0d1c565a4378bbcdb79fd6572add30643e4dc9a4
Sharlayan <118798881+sharlayan@users.noreply.github.com>
2026-07-22 21:24:11 +0000

    [Sharlayan] Add icon source

--- app/javascript/flavours/glitch/components/software_icon.js
+++ app/javascript/flavours/glitch/components/software_icon.js
@@ -1,17 +1,30 @@
 import mastodonIcon from '../../../images/logo-symbol-icon.svg';
 
+// Source: https://codeberg.org/catodon/catodon
 import catodonIcon from 'flavours/glitch/images/software/catodon.svg';
+// Source: https://github.com/kokonect-link/cherrypick
 import cherrypickIcon from 'flavours/glitch/images/software/cherrypick.svg';
+// Source: https://commons.wikimedia.org/wiki/File:Fediverse_logo_proposal.svg
 import fediverseIcon from 'flavours/glitch/images/software/fediverse.svg';
+// Source: https://github.com/fedify-dev/fedify
 import fedifyIcon from 'flavours/glitch/images/software/fedify.svg';
+// Source: https://akkoma.dev/FoundKeyGang/FoundKey
 import foundkeyIcon from 'flavours/glitch/images/software/foundkey.svg';
+// Source: https://github.com/hackers-pub/hackerspub
 import hackersPubIcon from 'flavours/glitch/images/software/hackerspub.svg';
+// Source: https://github.com/fedify-dev/hollo
 import holloIcon from 'flavours/glitch/images/software/hollo.svg';
+// Source: https://iceshrimp.dev/iceshrimp/iceshrimp
 import iceshrimpIcon from 'flavours/glitch/images/software/iceshrimp.svg';
+// Source: https://github.com/misskey-dev/misskey
 import misskeyIcon from 'flavours/glitch/images/software/misskey.svg';
+// Source: https://github.com/Chocobozzz/PeerTube
 import peertubeIcon from 'flavours/glitch/images/software/peertube.svg';
+// Source: https://github.com/pixelfed/pixelfed
 import pixelfedIcon from 'flavours/glitch/images/software/pixelfed.svg';
+// Source: https://git.pleroma.social/pleroma/pleroma
 import pleromaIcon from 'flavours/glitch/images/software/pleroma.svg';
+// Source: https://activitypub.software/TransFem-org/Sharkey
 import sharkeyIcon from 'flavours/glitch/images/software/sharkey.svg';
 
 const SOFTWARE_ICONS = {
