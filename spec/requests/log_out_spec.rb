53d08b739bbcdfdd2b8b540a96147513d0925ac7
Sharlayan <118798881+sharlayan@users.noreply.github.com>
2026-07-31 15:54:43 +0900

    [Sharlayan] Fix: return to main account on linked account logout

--- spec/requests/log_out_spec.rb
+++ spec/requests/log_out_spec.rb
@@ -26,5 +26,34 @@ RSpec.describe 'Log Out' do
 
       expect(response.parsed_body[:redirect_to]).to eq '/auth/sign_in'
     end
+
+    context 'when switched into a linked account' do
+      let(:linked_user) { Fabricate(:user) }
+
+      before do
+        Fabricate(:account_switch_authorization, account: user.account, target_account: linked_user.account)
+        post switch_account_path, params: { switch_to: linked_user.account.id }
+      end
+
+      it 'returns to the main account instead of logging out' do
+        delete '/auth/sign_out'
+
+        expect(response).to redirect_to(root_path)
+        expect(cookies[:switch_parent_stack]).to be_blank
+
+        post switch_account_path, params: { switch_to: linked_user.account.id }
+        expect(response).to redirect_to(root_path)
+        expect(cookies[:switch_parent_stack]).to be_present
+      end
+
+      it 'returns the main page redirect with a JSON request' do
+        delete '/auth/sign_out', headers: { 'HTTP_ACCEPT' => 'application/json' }
+
+        expect(response).to have_http_status(200)
+        expect(response.media_type).to eq 'application/json'
+        expect(response.parsed_body[:redirect_to]).to eq root_path
+        expect(cookies[:switch_parent_stack]).to be_blank
+      end
+    end
   end
 end
