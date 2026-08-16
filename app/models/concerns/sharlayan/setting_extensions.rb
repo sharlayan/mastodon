f4bd045853882254f33292df1a8b517503d99b2e
Sharlayan <118798881+sharlayan@users.noreply.github.com>
2026-08-16 02:32:11 +0000

    [Sharlayan] Enforce minimum status character limit

--- app/models/concerns/sharlayan/setting_extensions.rb
+++ app/models/concerns/sharlayan/setting_extensions.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Sharlayan::SettingExtensions
+  MIN_STATUS_CHARACTER_LIMIT = 300
+
   module_function
 
   def merge_defaults(defaults)
@@ -10,7 +12,8 @@ module Sharlayan::SettingExtensions
   def sharlayan_defaults
     content = Rails.root.join('config', 'settings', 'sharlayan.yml').read
     defaults = YAML.safe_load(content).fetch('defaults')
-    defaults['status_character_limit'] = ENV.fetch('MAX_TOOT_CHARS', defaults.fetch('status_character_limit')).to_i
+    configured_status_limit = Integer(ENV.fetch('MAX_TOOT_CHARS', ''), exception: false)
+    defaults['status_character_limit'] = configured_status_limit if configured_status_limit&.>= MIN_STATUS_CHARACTER_LIMIT
     defaults['profile_fields_limit'] = ENV.fetch('MAX_PROFILE_FIELDS', defaults.fetch('profile_fields_limit')).to_i.clamp(0, ActivityPub::ProcessAccountService::MAX_PROFILE_FIELDS)
     defaults['remote_media_attachments_limit'] = ENV.fetch('REMOTE_MEDIA_ATTACHMENTS_LIMIT', defaults.fetch('remote_media_attachments_limit')).to_i.clamp(Status::MEDIA_ATTACHMENTS_LIMIT, 16)
     defaults
