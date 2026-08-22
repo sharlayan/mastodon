420f55d4385e6cc4d69410e10831a379638dd129
Sharlayan <118798881+sharlayan@users.noreply.github.com>
2026-08-22 10:28:28 +0900

    [Sharlayan] Theme-fix: Fix missing BirdSiteUI menu and more icons

--- app/javascript/flavours/glitch/features/navigation_panel/components/more_link.tsx
+++ app/javascript/flavours/glitch/features/navigation_panel/components/more_link.tsx
@@ -127,7 +127,11 @@ export const MoreLink: React.FC = () => {
   return (
     <Dropdown items={menu} placement='bottom-start'>
       <button className='column-link column-link--transparent' type='button'>
-        <Icon id='' icon={MoreHorizIcon} className='column-link__icon' />
+        <Icon
+          id='ellipsis-h'
+          icon={MoreHorizIcon}
+          className='column-link__icon'
+        />
 
         <FormattedMessage id='navigation_bar.more' defaultMessage='More' />
       </button>
